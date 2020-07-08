a72695bcaddf87b4d6993f0f8ab7cebbd2636f9b
rafael.dyszy <rafaeldyszy@hotmail.com>
2020-07-08 18:31:15 -0400

    Adiciona rota de put

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -46,4 +46,17 @@ app.patch('/student/:id', async (req, res) => {
   }
 });
 
+app.put('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 export { app as studentRouter };
