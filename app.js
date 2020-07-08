9dc938292a15f2b01657b5c676cb6c88c23b5be4
rafael.dyszy <rafaeldyszy@hotmail.com>
2020-07-08 18:19:24 -0400

    "commit inicial"

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { studentRouter } from './routes/studentRoutes.js';
+
+// concetion DB
+(async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://igti:rafaeldyszy@cluster0-jfolz.mongodb.net/grades',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (err) {
+    console.log('Erro ao concetar');
+  }
+})();
+
+const app = express();
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(3000, () => console.log('API iniciada'));
---
+++ routes/studentRoutes.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { studentModel } from '../models/studentModels.js';
+const app = express();
+
+app.get('/student', async (req, res) => {
+  try {
+    const student = await studentModel.find({});
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post('/student', async (req, res) => {
+  try {
+    const student = new studentModel(req.body);
+    await student.save();
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.delete('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndDelete(req.params.id);
+    if (!student) {
+      res.status(404).send('Documento nao encontrado!');
+    }
+    res.status(200).send('Excluido com sucesso!');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.patch('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export { app as studentRouter };
